83b044f5ceef1bb22d8729425d87fe204c6f83dc
knees <you@example.com>
2018-03-11 08:26:43 -0700

    imageonclickworking

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,20 +1,20 @@
-var topics = ["apple","salad","quinoa","rice","tofu","noodles","broccoli","chicken","burrito","hamburger"]
+var topics = ["apple", "salad", "quinoa", "rice", "tofu", "noodles", "broccoli", "chicken", "burrito", "hamburger"]
 
-function newButtons(){
+function newButtons() {
 
-		$("#foodButtons").empty();
+	$("#foodButtons").empty();
 
-for (var i = 0; i < topics.length; i++) {
-	var newButton = $('<button>');
-	newButton.addClass("foodButton")
-	newButton.attr("data-topics", topics[i]);
-	newButton.text(topics[i]);
-	$("#foodButtons").append(newButton);
-	
+	for (var i = 0; i < topics.length; i++) {
+		var newButton = $('<button>');
+		newButton.addClass("foodButton")
+		newButton.attr("data-topics", topics[i]);
+		newButton.text(topics[i]);
+		$("#foodButtons").append(newButton);
 
+
+	}
 }
-}
-$("#addFood").on("click", function(event) {
+$("#addFood").on("click", function (event) {
 	event.preventDefault();
 
 	var newFood = $("#foodInput").val().trim();
@@ -24,49 +24,49 @@ $("#addFood").on("click", function(event) {
 });
 newButtons();
 
-$(".foodButton").on("click", function() {
+$(".foodButton").on("click", function () {
 	$("#foodGifsHere").empty();
 	var foodChosen = $(this).attr("data-topics");
 	console.log(foodChosen);
 
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q="
-	+ foodChosen +"&api_key=MpllQpfM1mnizCgb6mPiIBCR9x3e3RRq&limit=10&rating=pg-13"
+		+ foodChosen + "&api_key=MpllQpfM1mnizCgb6mPiIBCR9x3e3RRq&limit=10&rating=pg-13"
 	console.log(queryURL);
 	$.ajax({
 		url: queryURL,
 		method: "GET"
 	})
-	.then(function(response){
-		console.log(response);
-		var results = response.data;
-		for (var i = 0;i < results.length; i++) {
-			var foodDiv = $("<div>");
-			var p = $("<p>").text("Rating: "+ results[i].rating);
-			var foodImage = $("<img>");
-			foodImage.addClass("foodGiphys");
-			foodImage.attr("src", results[i].images.fixed_height_still.url);
-			foodImage.attr("data-still", results[i].images.fixed_height_still.url);
-			foodImage.attr("data-animate", results[i].images.fixed_height.url);
-			foodImage.attr("data-state","still");
-			foodDiv.append(p);
-			foodDiv.append(foodImage);
-			$("#foodGifsHere").append(foodDiv);
-		}
+		.then(function (response) {
+			console.log(response);
+			var results = response.data;
+			for (var i = 0; i < results.length; i++) {
+				var foodDiv = $("<div>");
+				var p = $("<p>").text("Rating: " + results[i].rating);
+				var foodImage = $("<img>");
+				foodImage.addClass("foodGiphys");
+				foodImage.attr("src", results[i].images.fixed_height_still.url);
+				foodImage.attr("data-still", results[i].images.fixed_height_still.url);
+				foodImage.attr("data-animate", results[i].images.fixed_height.url);
+				foodImage.attr("data-state", "still");
+				foodDiv.append(p);
+				foodDiv.append(foodImage);
+				$("#foodGifsHere").append(foodDiv);
+			}
 
-	})
+		})
 });
 
-$(".foodGiphys").on("click", function() {
+$(".container").on('click', ".foodGiphys" ,function () {
 	console.log("turtle");
-//	var state = $(this).attr("data-state");
+	//	var state = $(this).attr("data-state");
 
-//	var state = $(this).attr("data-state");
-//		if(state == "still") {
-//			$(this).attr("src", $(this).attr("data-animate"));
-//			$(this).attr("data-state", "moving");
-//		} else {
-//			$(this).attr("src", $(this).attr("data-still"));
-//			$(this).attr("data-state", "still");
-//		}
+	//	var state = $(this).attr("data-state");
+	//		if(state == "still") {
+	//			$(this).attr("src", $(this).attr("data-animate"));
+	//			$(this).attr("data-state", "moving");
+	//		} else {
+	//			$(this).attr("src", $(this).attr("data-still"));
+	//			$(this).attr("data-state", "still");
+	//		}
 
 });
