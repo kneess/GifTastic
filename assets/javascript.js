09194a1222ab84d59bb8bede6347368ba53058a2
knees <you@example.com>
2018-03-10 21:26:06 -0700

    forgottopush

--- assets/javascript.js
+++ assets/javascript.js
@@ -10,6 +10,7 @@ for (var i = 0; i < topics.length; i++) {
 	newButton.attr("data-topics", topics[i]);
 	newButton.text(topics[i]);
 	$("#foodButtons").append(newButton);
+	
 
 }
 }
@@ -23,17 +24,49 @@ $("#addFood").on("click", function(event) {
 });
 newButtons();
 
-$("#foodButtons").on("click", function() {
+$(".foodButton").on("click", function() {
+	$("#foodGifsHere").empty();
 	var foodChosen = $(this).attr("data-topics");
+	console.log(foodChosen);
 
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q="
-	+ "quinoa" +"&api_key=dc6zaTOxFJmzC"
+	+ foodChosen +"&api_key=MpllQpfM1mnizCgb6mPiIBCR9x3e3RRq&limit=10&rating=pg-13"
 	console.log(queryURL);
 	$.ajax({
-		URL: queryURL,
+		url: queryURL,
 		method: "GET"
 	})
 	.then(function(response){
 		console.log(response);
+		var results = response.data;
+		for (var i = 0;i < results.length; i++) {
+			var foodDiv = $("<div>");
+			var p = $("<p>").text("Rating: "+ results[i].rating);
+			var foodImage = $("<img>");
+			foodImage.addClass("foodGiphys");
+			foodImage.attr("src", results[i].images.fixed_height_still.url);
+			foodImage.attr("data-still", results[i].images.fixed_height_still.url);
+			foodImage.attr("data-animate", results[i].images.fixed_height.url);
+			foodImage.attr("data-state","still");
+			foodDiv.append(p);
+			foodDiv.append(foodImage);
+			$("#foodGifsHere").append(foodDiv);
+		}
+
 	})
-})
+});
+
+$(".foodGiphys").on("click", function() {
+	console.log("turtle");
+//	var state = $(this).attr("data-state");
+
+//	var state = $(this).attr("data-state");
+//		if(state == "still") {
+//			$(this).attr("src", $(this).attr("data-animate"));
+//			$(this).attr("data-state", "moving");
+//		} else {
+//			$(this).attr("src", $(this).attr("data-still"));
+//			$(this).attr("data-state", "still");
+//		}
+
+});
